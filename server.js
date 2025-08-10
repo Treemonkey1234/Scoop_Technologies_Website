3bacd21fb9038698e44ef065bf64c4d621c7625d
Treemonkey1234 <treemonkey1234@github.com>
2025-08-10 14:35:57 -0500

    Backend: root server.js delegating to Scoop_Social_MVP/server.js for DO App Platform

---
+++ server.js
@@ -0,0 +1,5 @@
+// Bootstraps backend by delegating to the app server in subdirectory.
+// DigitalOcean App Platform runs `node server.js` from repo root.
+require('./Scoop_Social_MVP/server.js')
+
+
